2b550fac8bb1be068b8a108fbc319396c5c5282c
Roshan <developerroshan01@gmail.com>
2024-07-24 14:32:00 +0545

    Initial Commit

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def encode_faces(imgs):
         small_img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
         img_rgb = cv2.cvtColor(small_img, cv2.COLOR_BGR2RGB)
         face_encodings = fr.face_encodings(img_rgb)
-        encode_list.append(face_encodings)
+        encode_list.append(face_encodings[0])
     return encode_list
 
 
